c737b3318f262e03c1982d2d9db8eca2c9100e51
Philip Chang <philip@ucsd.edu>
2023-02-19 03:07:33 -0800

    :blue_book: Update the path to blue in writetree.cc which was forgotten and also add TNamed objects from parent to new ones.

--- gnn/writetree.cc
+++ gnn/writetree.cc
@@ -1,12 +1,15 @@
 #include "csv.h"
 #include "TFile.h"
 #include "TTree.h"
+#include "TNamed.h"
+#include "TString.h"
 #include <iostream>
 #include <vector>
 
 int main()
 {
-    io::CSVReader<3> inference("/home/p.chang/data/lst/GATOR/inference/data_hiddensize200_lr0.005_epoch50.csv");
+    TString csv_path = "/blue/p.chang/p.chang/data/lst/GATOR/inference/data_hiddensize200_lr0.005_epoch50.csv";
+    io::CSVReader<3> inference(csv_path.Data());
     std::vector<std::vector<int>> eventid_vec_vec;
     std::vector<std::vector<float>> istrue_vec_vec;
     std::vector<std::vector<float>> score_vec_vec;
@@ -41,7 +44,7 @@ int main()
     istrue_vec.clear();
     score_vec.clear();
 
-    TFile* file = new TFile("/home/p.chang/data/lst/CMSSW_12_2_0_pre2/LSTGnnNtuple_ttbar_PU200.root");
+    TFile* file = new TFile("/blue/p.chang/p.chang/data/lst/CMSSW_12_2_0_pre2/LSTGnnNtuple_ttbar_PU200.root");
     TTree* tree = (TTree*) file->Get("tree");
 
     TFile* ofile = new TFile("output.root", "recreate");
@@ -63,6 +66,23 @@ int main()
         this_event++;
         outtree->Fill();
     }
+
+    // handling extra metadata information
+    TNamed code_tag_data("code_tag_data", file->Get("code_tag_data")->GetTitle());
+    TNamed make_log("make_log", file->Get("make_log")->GetTitle());
+    TNamed gitdiff("gitdiff", file->Get("gitdiff")->GetTitle());
+    TNamed input("input", file->Get("input")->GetTitle());
+    TNamed full_cmd_line("full_cmd_line", file->Get("full_cmd_line")->GetTitle());
+    TNamed tracklooper_path("tracklooper_path", file->Get("tracklooper_path")->GetTitle());
+    TNamed gnn_csv_path("gnn_csv_path", csv_path.Data());
+
+    code_tag_data.Write();
+    make_log.Write();
+    gitdiff.Write();
+    input.Write();
+    full_cmd_line.Write();
+    tracklooper_path.Write();
+    gnn_csv_path.Write();
 
     outtree->Write();
     ofile->Close();
