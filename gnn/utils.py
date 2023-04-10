38ecf022cb30a31ec2393c1d3f02b04008c53434
Jonathan Guiang <jkguiang@gmail.com>
2023-04-10 14:34:29 -0400

    added printing utility, extra features for GatorConfig

--- gnn/utils.py
+++ gnn/utils.py
@@ -2,11 +2,6 @@ import json
 from types import SimpleNamespace
 
 class GatorConfig(SimpleNamespace):
-    """
-    Class that imports GNN configs
-
-    TODO: add validation
-    """
     @classmethod
     def from_json(cls, config_json):
         with open(config_json, "r") as f:
@@ -14,3 +9,28 @@ class GatorConfig(SimpleNamespace):
 
     def get(self, key, default=None):
         return self.__dict__.get(key, default)
+
+    def keys(self):
+        return self.__dict__.keys()
+
+    def items(self):
+        return self.__dict__.items()
+
+    def __get_json(self, d):
+        json = {}
+        for key, val in d.items():
+            if type(val) == type(self):
+                json[key] = self.__get_json(val)
+            else:
+                json[key] = val
+        return json
+
+    def __str__(self):
+        return json.dumps(self.__get_json(self.__dict__), indent=4)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+def print_title(text):
+    text = f" {text} "
+    print(f"{text:-^50}", flush=True)
