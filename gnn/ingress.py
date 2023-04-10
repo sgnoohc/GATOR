added loading bars, fixed feature normalization

gnn/ingress.py
@@ -2,20 +2,24 @@
 
 import os
 import argparse
+
 import uproot
-import numpy as np
 import torch
 from torch_geometric.data import Data
 from torch_geometric.utils import to_undirected
+from tqdm import tqdm
 
 from utils import GatorConfig
 
-def ingress(entry_start, entry_stop, config, tag, save=True):
+def ingress(set_name, config, save=True):
+
+    start, stop = config.ingress.get(f"{set_name}_entry_range")
 
     tree = uproot.open(f"{config.ingress.input_file}:{config.ingress.ttree_name}")
 
+    print(f"Loaded input file, reading {start} to {stop} for {set_name} set")
     data_list = []
-    for batch in tree.iterate(step_size=1, filter_name=config.ingress.get("branch_filter", None), entry_start=entry_start, entry_stop=entry_stop):
+    for batch in tqdm(tree.iterate(step_size=1, filter_name=config.ingress.get("branch_filter", None), entry_start=start, entry_stop=stop), total=(stop - start)):
 
         batch = batch[0,:] # only one event per batch
 
@@ -24,29 +28,32 @@ def ingress(entry_start, entry_stop, config, tag, save=True):
 
         # Get edge features
         edge_attr = []
-        # TEMP START: temp solution for bug in LST output
         for branch_name in config.ingress.edge_features:
-            feature = batch[branch_name].to_numpy()
-            feature[np.isinf(feature)] = 1000
-            edge_attr.append(list(feature))
-        edge_attr = torch.tensor(edge_attr, dtype=torch.float)
-        edge_attr = torch.transpose(edge_attr, 0, 1)
-        edge_attr -= edge_attr.min(1, keepdim=True)[0]
-        edge_attr /= edge_attr.max(1, keepdim=True)[0]
-        # TEMP END
-        # edge_attr = torch.tensor([batch[n].to_list() for n in config.ingress.edge_features], dtype=torch.float)
-        # edge_attr = torch.transpose(edge_attr, 0, 1)
+            feature = torch.tensor(batch[branch_name].to_list(), dtype=torch.float)
+            feature[torch.isinf(feature)] = 1000
+            feature -= feature.min()
+            feature /= feature.max()
+            edge_attr.append(feature)
+
+        edge_attr = torch.transpose(torch.stack(edge_attr), 0, 1)
 
         # Get node features
-        node_attr = torch.tensor([batch[n].to_list() for n in config.ingress.node_features], dtype=torch.float)
-        node_attr = torch.transpose(node_attr, 0, 1)
-        node_attr -= node_attr.min(1, keepdim=True)[0]
-        node_attr /= node_attr.max(1, keepdim=True)[0]
+        node_attr = []
+        for branch_name in config.ingress.node_features:
+            feature = torch.tensor(batch[branch_name].to_list(), dtype=torch.float)
+            feature -= feature.min()
+            feature /= feature.max()
+            node_attr.append(feature)
+            
+        node_attr = torch.transpose(torch.stack(node_attr), 0, 1)
+        # node_attr = torch.tensor([batch[n].to_list() for n in config.ingress.node_features], dtype=torch.float)
+        # node_attr -= node_attr.min(1, keepdim=True)[0]
+        # node_attr /= node_attr.max(1, keepdim=True)[0]
 
         # Get truth labels
         truth = torch.tensor(~(batch[config.ingress.truth_label].to_numpy().astype(bool)), dtype=torch.float)
 
-        if config.ingress.undirected:
+        if config.ingress.get("undirected", False):
             edge_idxs_bi, edge_attr_bi = to_undirected(edge_idxs, edge_attr)
             _, truth_bi = to_undirected(edge_idxs, truth)
             data = Data(x=node_attr, y=truth_bi, edge_index=edge_idxs_bi, edge_attr=edge_attr_bi)
@@ -56,7 +63,7 @@ def ingress(entry_start, entry_stop, config, tag, save=True):
         data_list.append(data)
 
     if save:
-        outfile = f"{config.basedir}/{config.name}_{tag}.pt"
+        outfile = f"{config.basedir}/{config.name}_{set_name}.pt"
         torch.save(data_list, outfile)
         print(f"Wrote {outfile}")
 
@@ -71,6 +78,6 @@ if __name__ == "__main__":
     config = GatorConfig.from_json(args.config_json)
     os.makedirs(config.basedir, exist_ok=True)
 
-    ingress(0, 150, config, "train")
-    ingress(150, 160, config, "test")
-    ingress(160, 170, config, "val")
+    ingress("train", config)
+    ingress("test", config)
+    ingress("val", config)