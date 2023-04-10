db2f4f70cef18387cc1425a52ca1f242ff81c48e
Jonathan Guiang <jkguiang@gmail.com>
2023-04-10 14:34:18 -0400

    added DNN

--- gnn/models.py
+++ gnn/models.py
@@ -8,7 +8,7 @@ from torch_geometric.data import Data, DataLoader
 from torch_geometric.nn import MessagePassing
 from torch.nn import Sequential as Seq, Linear, ReLU, Sigmoid
 
-from utils import GatorConfig
+from utils import GatorConfig, print_title
 
 class RelationalModel(nn.Module):
     def __init__(self, input_size, output_size, n_hidden_layers, hidden_size):
@@ -67,7 +67,7 @@ class InteractionNetwork(MessagePassing):
         for round_i in range(self.n_msgpass_rounds - 1):
             x_latent = self.propagate(edge_index, x=x_latent, edge_attr=edge_attr, size=None)
 
-        m2 = torch.cat([x_latent[edge_index[1]], x_latent[edge_index[0]], self.msg], dim=1)
+        m2 = torch.cat([x_latent[edge_index[1]], x_latent[edge_index[0]], edge_attr], dim=1)
 
         return torch.sigmoid(self.edge_classifier(m2))
 
@@ -84,16 +84,44 @@ class InteractionNetwork(MessagePassing):
         c = torch.cat([x, aggr_out], dim=1)
         return self.readout_mlp(c)
 
+class DNN(nn.Module):
+    def __init__(self, config: GatorConfig):
+        super().__init__()
+        n_edge_features = len(config.ingress.edge_features)
+        n_node_features = len(config.ingress.node_features)
+        input_size = 2*n_node_features + n_edge_features
+        n_hidden_layers = config.model.get("n_hidden_layers", 1)
+        hidden_size = config.model.get("hidden_size", 64)
+
+        hidden_layers = []
+        for layer_i in range(n_hidden_layers):
+            hidden_layers.append(nn.Linear(hidden_size, hidden_size))
+            hidden_layers.append(nn.ReLU())
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            *hidden_layers,
+            nn.Linear(hidden_size, 1),
+            nn.Sigmoid()
+        )
+
+        print("---- DNN Config ----")
+        print(self)
+
+    def forward(self, node_attr, edge_idxs, edge_attr):
+        return self.layers(torch.cat((node_attr, edge_attr), dim=1)).unsqueeze(1)
+
 class ChangNet(InteractionNetwork):
     def __init__(self, config: GatorConfig):
         n_edge_features = len(config.ingress.edge_features)
         n_node_features = len(config.ingress.node_features)
-        n_msg_features = config.model.get("n_message_features", 17)
+        n_msg_features = config.model.get("n_message_features", n_edge_features)
         n_lnode_features = config.model.get("n_latent_node_features", n_node_features)
         n_hidden_layers = config.model.get("n_hidden_layers", 1)
         hidden_size = config.model.get("hidden_size", 200)
 
-        print("---- ChangNet Config ----")
+        print_title("ChangNet Config")
         # Message calculation model (Node, Edge, Node) -> (Message)
         # i.e. "message function"
         message_mlp = RelationalModel(
@@ -102,8 +130,7 @@ class ChangNet(InteractionNetwork):
             n_hidden_layers,
             hidden_size
         )
-        print("Message MLP:")
-        print(message_mlp)
+        print(f"Message MLP = {message_mlp}")
 
         # Latent node calculation model (Node, Sum_i Message_i) -> (Latent Node)
         # i.e. "readout function"
@@ -113,18 +140,17 @@ class ChangNet(InteractionNetwork):
             n_hidden_layers,
             hidden_size
         )
-        print("Readout MLP:")
-        print(readout_mlp)
+        print(f"Readout MLP = {readout_mlp}")
 
         # Edge classification model (Latent Node, Message, Latent Node) -> (1 dim edge score)
         edge_classifier = RelationalModel(
-            2*n_lnode_features + n_msg_features,
+            # 2*n_lnode_features + n_msg_features,
+            2*n_lnode_features + n_edge_features,
             1,
             n_hidden_layers,
             hidden_size
         )
-        print("Edge classifier:")
-        print(edge_classifier)
+        print(f"Edge classifier = {edge_classifier}")
 
         super().__init__(
             message_mlp, 
