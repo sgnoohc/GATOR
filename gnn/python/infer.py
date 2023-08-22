9c95e22cc109e101ccd05768670bd01e1f6a77e6
Jonathan Guiang <jkguiang@gmail.com>
2023-08-22 13:41:27 -0400

    added options to run inference on extra root files or pt files

--- gnn/python/infer.py
+++ gnn/python/infer.py
@@ -6,12 +6,13 @@ from time import time
 import torch
 
 import models
+from ingress import ingress_file
 from utils import GatorConfig, SimpleProgress
 
 def infer(model, device, loader, output_csv):
-    csv_rows = ["event,edge_idx,label,score,node0_idx,node1_idx"]
+    csv_rows = ["batch,edge_idx,label,score,node0_idx,node1_idx"]
     times = []
-    for event_i, data in enumerate(SimpleProgress(loader)):
+    for batch_i, data in enumerate(SimpleProgress(loader)):
         data = data.to(device)
 
         start = time()
@@ -26,7 +27,7 @@ def infer(model, device, loader, output_csv):
             data.edge_index[1]      # node 1 index
         )
         for LS_i, (truth, score, n0, n1) in enumerate(zip(*data_to_save)):
-            csv_rows.append(f"{event_i},{LS_i},{int(truth)},{float(score)},{int(n0)},{int(n1)}")
+            csv_rows.append(f"{batch_i},{LS_i},{int(truth)},{float(score)},{int(n0)},{int(n1)}")
 
     with open(output_csv, "w") as f:
         f.write("\n".join(csv_rows))
@@ -42,6 +43,14 @@ if __name__ == "__main__":
         "--epoch", type=int, default=50, metavar="N",
         help="training epoch of model to use for inference (default: 50)"
     )
+    parser.add_argument(
+        "--root_files", type=str, default="",
+        help="comma-separated list of .root files to run inferences over"
+    )
+    parser.add_argument(
+        "--pt_files", type=str, default="",
+        help="comma-separated list of .pt files to run inferences over"
+    )
     args = parser.parse_args()
 
     config = GatorConfig.from_json(args.config_json)
@@ -56,32 +65,73 @@ if __name__ == "__main__":
     model.eval()
     model = model.to(device)
 
-    # Load test/train samples
-    print("Loading data...")
-    train_loader = torch.load(config.get_outfile(subdir="datasets", tag="train", short=True))
-    test_loader = torch.load(config.get_outfile(subdir="datasets", tag="test", short=True))
-    
     if "DNN" in config.model.name:
         from datasets import EdgeDataset, EdgeDataBatch
         from torch.utils.data import DataLoader
-        # Hacky PyG graph-level GNN inputs --> edge-level DNN inputs
-        train_loader = DataLoader(
-            EdgeDataset(train_loader), batch_size=10000, collate_fn=lambda batch: EdgeDataBatch(batch)
-        )
-        test_loader = DataLoader(
-            EdgeDataset(test_loader), batch_size=10000, collate_fn=lambda batch: EdgeDataBatch(batch)
-        )
 
     times = []
-    print("Running training inferences...")
-    times += infer(
-        model, device, train_loader, 
-        config.get_outfile(subdir="inferences", tag="train_inferences", epoch=args.epoch, ext="csv")
-    )
-    print("Running testing inferences...")
-    times += infer(
-        model, device, test_loader, 
-        config.get_outfile(subdir="inferences", tag="test_inferences", epoch=args.epoch, ext="csv")
-    )
+    if args.root_files:
+        for root_file in args.root_files.split(","):
+            root_file_name = root_file.split("/")[-1].replace(".root", "")
+            # Load sample
+            loader = ingress_file(config, root_file, save=False, plot=False)
+        
+            if "DNN" in config.model.name:
+                # Hacky PyG graph-level GNN inputs --> edge-level DNN inputs
+                loader = DataLoader(
+                    EdgeDataset(loader), batch_size=10000, collate_fn=lambda batch: EdgeDataBatch(batch)
+                )
+
+            # Run inferences
+            print("Running inferences...")
+            times += infer(
+                model, device, loader, 
+                config.get_outfile(subdir="inferences", tag=root_file_name, epoch=args.epoch, ext="csv")
+            )
+    if args.pt_files:
+        for pt_file in args.pt_files.split(","):
+            pt_file_name = pt_file.split("/")[-1].replace(".pt", "")
+            # Load sample
+            loader = torch.load(pt_file)
+        
+            if "DNN" in config.model.name:
+                # Hacky PyG graph-level GNN inputs --> edge-level DNN inputs
+                loader = DataLoader(
+                    EdgeDataset(loader), batch_size=10000, collate_fn=lambda batch: EdgeDataBatch(batch)
+                )
+
+            # Run inferences
+            print("Running inferences...")
+            times += infer(
+                model, device, loader, 
+                config.get_outfile(subdir="inferences", tag=pt_file_name, epoch=args.epoch, ext="csv")
+            )
+    if not args.root_files and not args.pt_files:
+        # Load test/train samples
+        print("Loading data...")
+        train_loader = torch.load(config.get_outfile(subdir="datasets", tag="train", short=True))
+        test_loader = torch.load(config.get_outfile(subdir="datasets", tag="test", short=True))
+        
+        if "DNN" in config.model.name:
+            # Hacky PyG graph-level GNN inputs --> edge-level DNN inputs
+            train_loader = DataLoader(
+                EdgeDataset(train_loader), batch_size=10000, collate_fn=lambda batch: EdgeDataBatch(batch)
+            )
+            test_loader = DataLoader(
+                EdgeDataset(test_loader), batch_size=10000, collate_fn=lambda batch: EdgeDataBatch(batch)
+            )
+
+        # Run inferences
+        print("Running training inferences...")
+        times += infer(
+            model, device, train_loader, 
+            config.get_outfile(subdir="inferences", tag="train_inferences", epoch=args.epoch, ext="csv")
+        )
+        print("Running testing inferences...")
+        times += infer(
+            model, device, test_loader, 
+            config.get_outfile(subdir="inferences", tag="test_inferences", epoch=args.epoch, ext="csv")
+        )
+
     print(f"Avg. inference time: {sum(times)/len(times)}s")
     print("\nDone.\n")
